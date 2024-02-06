468393fbfcc67a01bb39ad2ca65e8e33eb2ab8f8
vishal <vishalpote9090@gmail.com>
2024-02-06 11:44:27 +0530

    Add Video Model And Jwt Toke

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,9 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const userSchema=new mongoose.Schema({
     username:{
         type:String,
@@ -58,6 +62,33 @@ userSchema.pre("save",async function(next){
 userSchema.methods.checkPassword = async function(password){
     return await bcrypt.compare(password,this.password);
 }
+
+userSchema.methods.generateAccessToken = function(){
+        return jwt.sign({
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname,
+        },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+            }
+        )
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+        return jwt.sign({
+            _id:this._id,
+        },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+            }
+        )
+}
+
 
 const User=mongoose.model("User",userSchema);
 
