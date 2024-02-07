1ba3e6bd4824add892c2a286aea577f89deeb5db
vishal <vishalpote9090@gmail.com>
2024-02-07 18:21:52 +0530

    Start Testing Using Postman

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,8 +1,14 @@
-
+import User from "../models/user.model.js"
 
 
 export const userController=async(req,res)=>{
-    res.status(200).json({
-        message:"Ok"
-    })
+   const {username,email,fullname,password}=req.body;
+   if(!username || !email || !fullname || !password){
+        res.status(404).json({message:"Please Enter All The Fileds"});
+   }
+
+   const existed=User.findOne({$or:[{username},{email}]})
+   if(existed){
+        res.status(409).json({message:"Username And Email Already Exist.."});
+   }
 }
