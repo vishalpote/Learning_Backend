bf917b8d818af6d5c1e110e38d64514aeb0ae5ff
vishal <vishalpote9090@gmail.com>
2024-02-12 17:44:30 +0530

    add mongodb atlas cloud

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,14 @@ dotenv.config();
 const connection=async()=>{
     const database=process.env.MONGODB_URL
 
+    //i use the mongodb compass here but if u want to use mongo db atlas cloud you can use
+    // i also use mongo db atlas cloud
+    // const database=process.env.MONGODB_URI
+
     try {
+        //this is for the mongodb atlas cloud
+        //    await mongoose.connect(`${database}`);
+           //and this is for the mongodb compass
            await mongoose.connect(`${database}/youtube`);
            console.log("Mongo DB Connected Succesfully..!!");
     } catch (error) {
